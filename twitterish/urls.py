93505f51e99d46f746da7b267607801ac3e32a7e
Mikael <nilssonmikael90@gmail.com>
2020-01-19 20:04:29 +0100

    Changed order of paths

--- twitterish/urls.py
+++ twitterish/urls.py
@@ -1,18 +1,3 @@
-"""twitterish URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
@@ -23,8 +8,9 @@ from .apps.profiles import views
 
 urlpatterns = [
     path('', include('twitterish.apps.posts.urls')),
-    path('', include('twitterish.apps.profiles.urls')),
+    path('profiles', include('twitterish.apps.profiles.urls')),
     path('admin/', admin.site.urls),
+    path('profile/', include('django.contrib.auth.urls')),
     path(
         'login/',
         auth_views.LoginView.as_view(template_name='auth/login.html'),
@@ -34,6 +20,5 @@ urlpatterns = [
         'register',
         views.ProfileSignUpView.as_view(),
         name='register'
-    ),
-    path('profile/', include('django.contrib.auth.urls')),
+    )
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
